5d607e8a233797181cee2621ddf8e7585caf2303
sumkee <sumkee911@gmail.com>
2025-08-22 19:07:25 +0900

    update scripts

--- scripts/state_sync.sh
+++ scripts/state_sync.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 # ====== Config ======
-SNAP_RPC="${SNAP_RPC:-http://mainnet-rpc.paxinet.io/}"
-SNAP_RPC2="${SNAP_RPC2:-http://mainnet-rpc.paxinet.io/}"
+SNAP_RPC="${SNAP_RPC:-http://rpc.paxi.info/}"
+SNAP_RPC2="${SNAP_RPC2:-http://rpc.paxi.info/}"
 CONFIG_FILE="${CONFIG_FILE:-$HOME/paxid/paxi/config/config.toml}"
 
 # ====== Preflight checks ======
